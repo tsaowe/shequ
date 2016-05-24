545dd9b2ffd3286dbd6efe7c239de50d47315354
kxf <kxf@kxfdeMac-mini.local>
2016-05-24 21:11:45 +0800

    upd main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,71 @@
 
-    var todayCount =  $('#today_count').text();
-    $('#today_count').html(formatCurrency(todayCount));
 
-    var sumCount =  $('#sum_count').text();
-    $('#sum_count').html(formatCurrency(sumCount));
+function initData(){
+    $.ajax({
+        type: "post",
+        //async: false,
+        url: "../data/main.json",
+        //dataType:'jsonp',
+        //jsonp:'callback',
+        //jsonpCallback:'jsonpCallback',
+        success:function(res){
+            if(res.code === 0){
+                var data = res.data;
+                $('#today_count').html(formatCurrency(data.todayIncoming));
+                $('#sum_count').html(formatCurrency(data.totalIncoming));
+                $('#reset_count').html(formatCurrency(data.unCashingAmount));
+                if(data.cashingOpen){
+                    $('.rm-btn').css({'background-color':'#fc5c6c'});
+                    cashAble = true;
+                }
+            }
 
-    var resetCount = $('#reset_count').text();
-    $('#reset_count').html(formatCurrency(resetCount));
+        }
 
-    $('#turn_record').tap(function(){
-        location.href='record.html';
     });
+}
+
+var cashAble = false;
+initData();
+
+$('#turn_record').on('tap',function(){
+    location.href='record.html';
+});
+
+$('.rm-btn').on('tap',function () {
+    if(cashAble){
+        $('#dialog_confirm').css({'display':'block'});
+        $('#dia_reset').html(formatCurrency(888.33));
+        $('#dia_out').html(formatCurrency(200.34));
+    }
+
+});
+
+$('#turn_btn').on('tap',function () {
+    $.ajax({
+        type: "post",
+        //async: false,
+        url: "../data/main.json",
+        //dataType:'jsonp',
+        //jsonp:'callback',
+        //jsonpCallback:'gdd',
+        success:function(res){
+            if(res.code === 0){
+                $('#dialog_confirm').css({'display':'none'});
+                $('#dialog_ok').css({'display':'block'});
+            }else{
+                //提示转出失败，请再次尝试
+            }
+        }
+    });
+
+});
+
+$('#turn_cancle').on('tap',function () {
+    $('#dialog_confirm').css({'display':'none'});
+});
+
+$('#ok_btn').on('tap',function () {
+    $('#dialog_ok').css({'display':'none'});
+});
+
