3d77dc8fc59105c4565e841d66f231a438e42e9a
kxf <kxf@kxfdeMac-mini.local>
2016-05-27 13:53:19 +0800

    upd css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var gulpif = require('gulp-if');
+//var sprite = require('css-sprite');
+var sprity = require('sprity');
+
 
 
 gulp.task('sass', function () {
@@ -14,6 +18,34 @@ gulp.task('sass', function () {
 		}))
         //.pipe(cleanCSS())
         .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('sprites',function(){
+    sprity.src({
+     src:'./images/note/*.png',
+     name: 'note',                       //定义一个名称
+     style: '_note.scss',                //这是生成的样式文件
+     format: 'png',                      //png格式的图片
+     orientation: 'vertical',          //雪碧图合并的方向，也可以设置成垂直或水平
+     cssPath: '../images/sprite/',    //雪碧图的路径变量
+     //template: './sprite-tpl.mustache',  //scss生成的模板
+     processor: 'sass',                   //生成的样式文件的格式
+     'style-type':'scss'})
+     .pipe(gulpif('*.png',gulp.dest('./images/sprite/'),gulp.dest('./css/sprite/')))
+
+    sprity.src( {
+        src:'./images/red/*.png',
+        name: 'red',
+        style: '_red.scss',
+        format: 'png',
+        orientation: 'vertical',
+        cssPath: '../images/sprite/',
+        processor: 'sass',
+        'style-type':'scss'})
+    .pipe(gulpif('*.png',gulp.dest('./images/sprite/'),gulp.dest('./css/sprite/')))
+
+
+
 });
 
 gulp.task('sass:watch', function () {
--- js/record.js
+++ js/record.js
@@ -4,28 +4,34 @@ function initData(){
     $.ajax({
         type: "post",
         //async: false,
-        url: "http://172.20.16.81:8081/show/rest/ifans_redenvs/cashing_list",
-        dataType:'jsonp',
+        url: "../data/record.json",
+        dataType:'json',
         jsonp:'callback',
         //jsonpCallback:'jsonpCallback',
         success:function(res){
             if(res.code === 0){
-                var list = res.data;
-                var recordList = $('#record_list');
-                recordList.find('.item').remove();
-                $.each(list,function(i,data){
-                    var item = '<div class="item">'+
-                    '<div class="content">'+
-                    '<div class="gap"></div>'+
-                    '<div class="event">转出到聚美余额</div>'+
-                    '<div class="gap-time"></div>'+
-                    '<div class="time">'+TimeToDate(data.createTime)+'</div>'+
-                    '</div>'+
-                    '<div class="fr sum">-'+data.cashingAmount+'</div>'+
-                    '</div>';
-                    recordList.append(item);
+                if(res.data && res.data.length>0){
+                    var list = res.data;
+                    var recordList = $('#record_list');
+                    recordList.find('.item').remove();
+                    recordList.find('.re-block').remove();
+                    $.each(list,function(i,data){
+                        var item = '<div class="item">'+
+                            '<div class="content">'+
+                            '<div class="gap"></div>'+
+                            '<div class="event">转出到聚美余额</div>'+
+                            '<div class="gap-time"></div>'+
+                            '<div class="time">'+TimeToDate(data.createTime)+'</div>'+
+                            '</div>'+
+                            '<div class="fr sum">- '+formatCurrency(data.cashingAmount)+'</div>'+
+                            '</div>';
+                        recordList.append(item);
 
-                });
+                    });
+                }else{
+                    $('.no-record').css({'display':'block'});
+
+                }
             }else{
                 if(res.action == 'toast'){
                     showMsg(res.message);
