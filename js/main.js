fb7dce33c1ef7d53d3b928ede8904d6b304099f0
kxf <kxf@kxfdeMac-mini.local>
2016-06-02 10:36:10 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -60,24 +60,40 @@ $('#turn_btn').on('tap',function () {
     $('#turn_btn').text('转出中...').css({'background-color':'#fdbdc4'});
     $.ajax({
         type: "post",
-        url: "../data/main2.json",
+        async:true,
+        url: "../data/main.json",
         dataType:'json',
         //jsonp:'callback',
         success:function(res){
             if(res.code === 0){
-                initData();
+                $('#turn_btn').text('转出');
                 $('#dialog_confirm').css({'display':'none'});
                 $('#dialog_ok').css({'display':'block'});
+                initData();
             }else{
                 if(res.action == 'toast'){
                     showMsg(res.message);
                 }
+                $('#turn_btn').text('转出').css({'background-color':'#fc5c6c'});
+                isClick = false;
             }
         },
-        error:function(){
-            showMsg('转出失败，请再试试');
-        },
-        complete:function(){
+        error:function(jqXHR,textStatus, errorThrown){
+            switch(jqXHR.status)
+            {
+                case 403:
+                    showMsg('抱歉，您没有权限');
+                    break;
+                case 503:
+                    showMsg('请求超时');
+                    break;
+                case 0:
+                    showMsg('网络中断');
+                    break;
+                default:
+                    showMsg('转出失败，请再试试');
+
+            }
             $('#turn_btn').text('转出').css({'background-color':'#fc5c6c'});
             isClick = false;
         }
